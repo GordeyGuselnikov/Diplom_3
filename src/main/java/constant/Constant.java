0c3e3da6fa076a020087d05e4cef106ac316508d
Gordey Guselnikov <g.guselnikov@bi.zone>
2022-09-04 17:45:58 +0700

    add constant

---
+++ src/main/java/constant/Constant.java
@@ -0,0 +1,16 @@
+package constant;
+
+public interface Constant {
+
+    String URL = "https://stellarburgers.nomoreparties.site/";
+    String DEFAULT_EMAIL = "test-data@yandex.ru";
+    String DEFAULT_PASSWORD = "password";
+    String LOGIN_URL = "https://stellarburgers.nomoreparties.site/login";
+    String ACCOUNT_PROFILE_URL = "https://stellarburgers.nomoreparties.site/account/profile";
+    String INVALID_PASSWORD = "Некорректный пароль";
+    boolean EXPECTED_RESULT_TRUE = true;
+
+    String BUNS = "Булки";
+    String SAUCES = "Соусы";
+    String FILLINGS = "Начинки";
+}
